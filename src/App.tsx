d79e2e706266a29158e4d6eda31135a82c8f53e8
bair-ochirov <pactuc@gmail.com>
2024-02-06 11:04:33 +0800

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {Header} from './components/header.component';
 import {Text} from './components/text.component';
 import {FirstButton, SecondButton} from './components/button.component';
 
+
 function App() {
     return (
         <MainDiv>
@@ -14,11 +15,12 @@ function App() {
                 <Header/>
                 <Text/>
                 <ButtonsDiv>
-                    <FirstButton bgcColor={'#4E71FE'} textColor={'#fff'}/>
-                    <SecondButton textColor={'#4E71FE'}/>
+                    <FirstButton $bgcColor={'#4E71FE'} $textColor={'#fff'}/>
+                    <SecondButton $textColor={'#4E71FE'}/>
                 </ButtonsDiv>
             </ContentDiv>
         </MainDiv>
+
     );
 }
 
@@ -27,8 +29,9 @@ export default App;
 
 
 /*
-1) Есть контейнер div главный у карточки. В нем к заголовок, текст и кнопки в своем div кнопочном. Отступы от краев карточки сделать лучше padding у div главного? Отступы по вертикали между заголовком, текстом и кнопками делать у самих элементов или у к примеру div кнопочного?
+1) Есть контейнер div главный у карточки. В нем заголовок, текст и кнопки, которые в своем div кнопочном. Отступы по вертикали между заголовком, текстом и кнопками делать у самих элементов или у к примеру div кнопочного?
 2) Между блоком Photo и блоком ContentDiv задать маржин боттом у картинки или маржин топ у блока ContentDiv или сделать паддинг у ContentDiv?
 3) Также внизу у кнопок на макете есть зазор до низа карточки в 20px. Его лучше сделать паддинг у всей картоки в 10px + паддинг у блока ContentDiv в 10px?
-4) Почему лучше задать маржин боттон между элементами, к примеру между Header и Text, а не маржин топ?
+4) Почему лучше задать маржин боттон между элементами, к примеру между Header и Text, а не паддинг топ или боттом?
+5) почему не переименовывается везде переменная через shift + F6
 */
--- src/components/button.component.tsx
+++ src/components/button.component.tsx
@@ -1,34 +1,37 @@
 import styled from 'styled-components';
 
 type ButtonComponentPropsType = {
-    bgcColor?: string
-    textColor?: string
+    $bgcColor?: string
+    $textColor?: string
 }
+
 
 
 const ButtonComponent = styled.button<ButtonComponentPropsType>`
     border-radius: 5px;
-    background-color: ${props => props.bgcColor || 'transparent'};
+    background-color: ${props => props.$bgcColor || 'transparent'};
     width: 86px;
     height: 30px;
-    color: ${props => props.textColor || 'transparent'};
+    color: ${props => props.$textColor || 'transparent'};
     font-size: 10px;
     font-weight: 700;
     line-height: 20px;
     border: 2px solid #4E71FE;
-    
-    
+
+
     &:hover {
         transform: translateY(-5px);
     }
 `
 
 export function FirstButton(props: ButtonComponentPropsType) {
-    return(
-        <ButtonComponent bgcColor={props.bgcColor} textColor={props.textColor}>See more</ButtonComponent>
+    return (
+        <ButtonComponent $bgcColor={props.$bgcColor} $textColor={props.$textColor}>See more</ButtonComponent>
     )
-}export function SecondButton(props: ButtonComponentPropsType) {
-    return(
-        <ButtonComponent textColor={props.textColor}>Save</ButtonComponent>
+}
+
+export function SecondButton(props: ButtonComponentPropsType) {
+    return (
+        <ButtonComponent $textColor={props.$textColor}>Save</ButtonComponent>
     )
 }
